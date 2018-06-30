3c0fd149098696fcf08a3f0d22d9ad470ba78ff7
jzh16s@hotmail.com <Ultraman7>
2018-06-30 11:40:18 +0800

    Initial Commit

---
+++ SketchPlugin/SketchPlugin.h
@@ -0,0 +1,19 @@
+//
+//  SketchPlugin.h
+//  SketchPlugin
+//
+//  Created by Jiang,Zhenhua on 2018/6/30.
+//  Copyright © 2018年 Daubert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchPlugin.
+FOUNDATION_EXPORT double SketchPluginVersionNumber;
+
+//! Project version string for SketchPlugin.
+FOUNDATION_EXPORT const unsigned char SketchPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchPlugin/PublicHeader.h>
+
+
